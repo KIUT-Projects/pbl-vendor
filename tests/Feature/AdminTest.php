4536ac14fc9d840e14bcf8751848913c092eaeeb
UzSoftic <softuzb@gmail.com>
2023-03-20 18:52:08 +0500

    update test files

---
+++ tests/Feature/AdminTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_admin_dashboard()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user, 'web')->get('/dashboard')->assertStatus(200);
+    }
+}
---
+++ tests/Feature/PageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_main_page(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_login_page()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_register_page()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_forgot_password_page()
+    {
+        $response = $this->get('/forgot-password');
+
+        $response->assertStatus(200);
+    }
+}
